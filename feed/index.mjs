df7608ce5f70ffce5321ec5ebbc6886855d8e343
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-03-03 19:11:06 +0100

    Added checkboxes that filters out results when check based on if there is an image or reaction/comment in a post

--- feed/index.mjs
+++ feed/index.mjs
@@ -2,7 +2,8 @@ import { doFetch } from "../js/api/auth/doFetch.mjs";
 import { POSTS_URL } from "../js/api/constants.mjs";
 import { generateSinglePostHTML } from "../js/generateSinglePostHTML.mjs";
 
-var POST_HAS_IMAGE = false;
+let POST_HAS_IMAGE = false;
+let POST_HAS_REACTION = false;
 
 function displayPosts(posts){
     const postsDisplayContainer = document.querySelector("#posts-display-container");
@@ -16,7 +17,17 @@ function displayPosts(posts){
             } else {
                 return true;
             }
-        }).forEach((post) => {
+        })
+        .filter ((post) => {
+            if (POST_HAS_REACTION) {
+                if (post._count.reactions || post._count.comments) {
+                    return true;
+                }
+            } else {
+                return true;
+            }
+        })
+        .forEach((post) => {
         const postHTML = generateSinglePostHTML(post);
         postsDisplayContainer.appendChild(postHTML);
     })
@@ -31,6 +42,18 @@ async function getPosts() {
 }
 
 function main() {
+    getPosts();
+
+    const imageCheckbox = document.getElementById('imageCheckbox');
+    const reactionCheckbox = document.getElementById('reactionCheckbox');
+
+    imageCheckbox.addEventListener('change', updatePostsDisplay);
+    reactionCheckbox.addEventListener('change', updatePostsDisplay);
+}
+
+function updatePostsDisplay() {
+    POST_HAS_IMAGE = document.getElementById('imageCheckbox').checked;
+    POST_HAS_REACTION = document.getElementById('reactionCheckbox').checked;
     getPosts();
 }
 
